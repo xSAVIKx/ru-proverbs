f83af476720e8b7516d5552830a9efe571d0d199
Yurii Serhiichuk <savik.ne@gmail.com>
2020-10-05 22:08:50 +0300

    Add typings and reformat code.

--- ru_proverbs/generator.py
+++ ru_proverbs/generator.py
@@ -1,11 +1,12 @@
 from os import path
+from typing import List
 
 from textgenrnn import textgenrnn
 
 script_file_path = path.dirname(__file__) + path.sep
 
 
-def generate_proverb(temperature=0.25, max_gen_length=256):
+def generate_proverb(temperature: float = 0.25, max_gen_length: int = 256) -> List[str]:
     """Generates a new proverb.
 
     :param temperature: the RNN temperature
@@ -13,13 +14,17 @@ def generate_proverb(temperature=0.25, max_gen_length=256):
     :return: a new proverb
     :rtype: list of str
     """
-    textgen = textgenrnn(name='ru_proverbs_chars',
-                         config_path=script_file_path + 'ru_proverbs_chars_config.json',
-                         weights_path=script_file_path + 'ru_proverbs_chars_weights.hdf5',
-                         vocab_path=script_file_path + 'ru_proverbs_chars_vocab.json')
-    response = textgen.generate(n=1,
-                                temperature=[temperature],
-                                max_gen_length=max_gen_length,
-                                progress=False,
-                                return_as_list=True)
+    textgen: textgenrnn = textgenrnn(
+        name='ru_proverbs_chars',
+        config_path=script_file_path + 'ru_proverbs_chars_config.json',
+        weights_path=script_file_path + 'ru_proverbs_chars_weights.hdf5',
+        vocab_path=script_file_path + 'ru_proverbs_chars_vocab.json'
+    )
+    response = textgen.generate(
+        n=1,
+        temperature=[temperature],
+        max_gen_length=max_gen_length,
+        progress=False,
+        return_as_list=True
+    )
     return response
