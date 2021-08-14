ae5397c0644674bf75486ad81e2ddb92cc321e0e
Yurii Serhiichuk <savik.ne@gmail.com>
2021-08-14 18:41:11 +0300

    Reformat code with Black.

--- ru_proverbs/__main__.py
+++ ru_proverbs/__main__.py
@@ -21,5 +21,5 @@ def main():
     print(generate_proverb())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- ru_proverbs/generator.py
+++ ru_proverbs/generator.py
@@ -22,9 +22,7 @@ script_file_path = path.dirname(__file__) + path.sep
 
 
 def generate_proverb(
-        temperature: float = 0.25,
-        max_gen_length: int = 256,
-        number_of_proverbs: int = 1
+    temperature: float = 0.25, max_gen_length: int = 256, number_of_proverbs: int = 1
 ) -> List[str]:
     """Generates proverbs.
 
@@ -35,16 +33,16 @@ def generate_proverb(
     :rtype: list of str
     """
     textgen: textgenrnn = textgenrnn(
-        name='ru_proverbs_chars',
-        config_path=script_file_path + 'ru_proverbs_chars_config.json',
-        weights_path=script_file_path + 'ru_proverbs_chars_weights.hdf5',
-        vocab_path=script_file_path + 'ru_proverbs_chars_vocab.json'
+        name="ru_proverbs_chars",
+        config_path=script_file_path + "ru_proverbs_chars_config.json",
+        weights_path=script_file_path + "ru_proverbs_chars_weights.hdf5",
+        vocab_path=script_file_path + "ru_proverbs_chars_vocab.json",
     )
     response = textgen.generate(
         n=number_of_proverbs,
         temperature=[temperature],
         max_gen_length=max_gen_length,
         progress=False,
-        return_as_list=True
+        return_as_list=True,
     )
     return response
